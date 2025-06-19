9697ad7a2863e20f09adfd48a4f220ee05320c73
den4ik <denis.shatalov95@gmail.com>
2025-06-20 01:51:01 +0300

    hw7

--- src/server.js
+++ src/server.js
@@ -4,20 +4,29 @@ import cors from 'cors';
 import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
 import path from 'node:path';
+import * as fs from 'node:fs';
 
 import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import contactsRouter from "./routers/contacts.js";
 import authRouter from "./routers/auth.js";
 
+import swaggerUI from "swagger-ui-express";
+
 export async function setupServer() {
   const PORT = 3000;
   const app = express();
+
+  const SWAGGER_DOCS = JSON.parse(
+    fs.readFileSync(path.join('docs', 'swagger.json'), 'utf-8'),
+  );
 
   app.use('/photo', express.static(path.resolve('src', 'upload', 'photo')));
 
   app.use(cookieParser());
   app.use(cors());
+
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(SWAGGER_DOCS));
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
